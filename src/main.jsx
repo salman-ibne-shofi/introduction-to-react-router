b5d1d2e501f9c461f8028708b26afa0fecea09b2
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-07-13 08:32:27 +0600

    try to do nested route concepts

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
 import {
     createBrowserRouter,
     RouterProvider,
   } from "react-router-dom";
+import Home from './component/Home/Home.jsx';
+import About from './component/About/About.jsx';
+import Contact from './component/Contact/Contact.jsx';
 
   const router = createBrowserRouter([
     {
         path: '/',
-        element: <div>Hello from react router!!</div>
+        element: <Home></Home>,
+        children: [
+            {
+                path: '/about',
+                element: <About></About>
+            },
+            {
+                path: '/contact',
+                element: <Contact></Contact>
+            }
+        ]
     },
-    {
-        path: '/about',
-        element: <div>I am in the about page</div>
-    },
-    {
-        path: '/contact',
-        element: <div>Call me right now</div>
-    }
+    
   ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
