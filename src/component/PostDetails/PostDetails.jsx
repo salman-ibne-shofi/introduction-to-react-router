20f8e343d8eb4ea3d7b008c2440256d16d21cfff
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-07-17 04:03:04 +0600

    navigate on click usenavigate not found and useparams

--- src/component/PostDetails/PostDetails.jsx
+++ src/component/PostDetails/PostDetails.jsx
@@ -1,8 +1,17 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 
 const PostDetails = () => {
 	const post = useLoaderData();
+    const postId = useParams();
+	const navigate = useNavigate();
 	const { id, title, body } = post;
+
+    console.log(postId);
+
+	const handleGoBack = () => {
+		navigate(-1);
+	};
+
 	return (
 		<div>
 			<h3>Post details about: {id}</h3>
@@ -10,6 +19,7 @@ const PostDetails = () => {
 			<p>
 				<small>{body}</small>
 			</p>
+			<button onClick={handleGoBack}>Go back</button>
 		</div>
 	);
 };
